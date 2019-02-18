8e59218d258d6e13773d4e36ffa8c0902d3c71fb
nathan5580 <nathan5580@gmail.com>
2019-02-18 16:19:15 +0100

    some things

--- Pygame test/main.py
+++ Pygame test/main.py
@@ -1,3 +1,4 @@
+# C:\\Users\\natha\\OneDrive\\Documents\\GitHub\\Python-CESI\\Pygame test\\perso.png
 import pygame
 from pygame.locals import *
 
@@ -7,8 +8,13 @@ pygame.init()
 fenetre = pygame.display.set_mode((640, 480))
 
 #Chargement et collage du fond
-fond = pygame.image.load("C:\\Users\\natha\\OneDrive\\Documents\\GitHub\\Python-CESI\\Pygame test\\background.jpg").convert()
+fond = pygame.image.load("C:\\Users\\natha\\OneDrive\\Documents\\GitHub\\Python-CESI\\Pygame test\\hexa.png").convert()
 fenetre.blit(fond, (0,0))
+
+#Chargement et collage du personnage
+perso = pygame.image.load("C:\\Users\\natha\\OneDrive\\Documents\\GitHub\\Python-CESI\\Pygame test\\perso.png").convert_alpha()
+position_perso = perso.get_rect()
+fenetre.blit(perso, position_perso)
 
 #Rafraîchissement de l'écran
 pygame.display.flip()
@@ -16,4 +22,25 @@ pygame.display.flip()
 #BOUCLE INFINIE
 continuer = 1
 while continuer:
-	continuer = int(input())
+    for event in pygame.event.get():    #Attente des événements
+        if event.type == QUIT:
+            continuer = 0
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:    #Si "flèche bas"
+                #On descend le perso
+                position_perso = position_perso.move(0,3)
+            if event.key == K_UP:    #Si "flèche bas"
+                #On descend le perso
+                position_perso = position_perso.move(0,-3)
+            if event.key == K_RIGHT:    #Si "flèche bas"
+                #On descend le perso
+                position_perso = position_perso.move(3,0)
+            if event.key == K_LEFT:    #Si "flèche bas"
+                #On descend le perso
+                position_perso = position_perso.move(-3,0)
+
+    #Re-collage
+    fenetre.blit(fond, (0,0))
+    fenetre.blit(perso, position_perso)
+    #Rafraichissement
+    pygame.display.flip()
