9d2b0736387beca6b13fe786bd118c1abb8592f2
nathan5580 <nathan5580@gmail.com>
2019-02-19 16:25:55 +0100

    f

--- Socket Test/client.py
+++ Socket Test/client.py
@@ -1,17 +1,46 @@
 import socket
 
+#socket
 print("- Ready")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("- Socket initialized")
 s.connect(("127.0.0.1", 1111))
 print("- Socket connected to server")
+#socket
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((640, 480),RESIZABLE)
+
+#Chargement et collage du fond
+fond = pygame.image.load("C:\\Users\\natha\\OneDrive\\Documents\\GitHub\\Python-CESI\\Pygame test\\hexa.png").convert()
+fenetre.blit(fond, (0,0))
+
+#Rafraîchissement de l'écran
+pygame.display.flip()
+
+# Start Socket
 
 name = "john"
-
 s.send(name.encode())
-print("- Socket sent")
-
 r = s.recv(9999999)
-print("- Socket finished")
 
 print(r)
+
+# End Sockets
+
+#BOUCLE INFINIE
+continuer = 1
+while continuer:
+    for event in pygame.event.get():    #Attente des événements
+        if event.type == QUIT:
+            continuer = 0
+
+    #Re-collage
+    fenetre.blit(fond, (0,0))
+    #Rafraichissement
+    pygame.display.flip()
--- Socket Test/server.py
+++ Socket Test/server.py
@@ -1,34 +1,78 @@
 import socket
-import threading
+import sys
+import traceback
+from threading import Thread
 
-class ClientThread(threading.Thread):
 
-    def __init__(self, ip, port, clientsocket):
+def main():
+    start_server()
 
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.clientsocket = clientsocket
-        print("[+] Nouveau thread pour %s %s" % (self.ip, self.port, ))
 
-    def run(self): 
-    
-        print("Connexion de %s %s" % (self.ip, self.port, ))
+def start_server():
+    host = "127.0.0.1"
+    port = 1111
 
-        r = self.clientsocket.recv(2048)
-        print("User input: ", r)
-        message = "Lu par le serveur"
-        self.clientsocket.send(message.encode())
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("Socket created")
 
-        print("Client déconnecté...")
+    try:
+        soc.bind((host, port))
+    except:
+        print("Bind failed. Error : " + str(sys.exc_info()))
+        sys.exit()
 
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind(("127.0.0.1",1111))
+    soc.listen(5) # queue up to 5 requests
+    print("Socket now listening")
 
-while True:
-    tcpsock.listen(10)
-    print( "En écoute...")
-    (clientsocket, (ip, port)) = tcpsock.accept()
-    newthread = ClientThread(ip, port, clientsocket)
-    newthread.start()
+    # infinite loop- do not reset for every requests
+    while True:
+        connection, address = soc.accept()
+        ip, port = str(address[0]), str(address[1])
+        print("Connected with " + ip + ":" + port)
+
+        try:
+            Thread(target=client_thread, args=(connection, ip, port)).start()
+        except:
+            print("Thread did not start.")
+            traceback.print_exc()
+
+    soc.close()
+
+
+def client_thread(connection, ip, port, max_buffer_size = 5120):
+    is_active = True
+
+    while is_active:
+        client_input = receive_input(connection, max_buffer_size)
+
+        if "--QUIT--" in client_input:
+            print("Client is requesting to quit")
+            connection.close()
+            print("Connection " + ip + ":" + port + " closed")
+            is_active = False
+        else:
+            print("Processed result: {}".format(client_input))
+            connection.sendall("-".encode("utf8"))
+
+
+def receive_input(connection, max_buffer_size):
+    client_input = connection.recv(max_buffer_size)
+    client_input_size = sys.getsizeof(client_input)
+
+    if client_input_size > max_buffer_size:
+        print("The input size is greater than expected {}".format(client_input_size))
+
+    decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
+    result = process_input(decoded_input)
+
+    return result
+
+
+def process_input(input_str):
+    print("Processing the input received from client")
+
+    return str(input_str).upper()
+
+if __name__ == "__main__":
+    main()
